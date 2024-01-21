638868074acb75af80779bbfb199a18aa5f1d3c4
Barvinko <vlad.barvinko@gmail.com>
2024-01-22 01:21:15 +0200

    refactor: change "05-merge-styles" according to "preterite" rules

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -4,35 +4,35 @@ const path = require('path');
 const dirPath = path.join(__dirname, 'styles');
 
 (async () => {
-  try{
+  try {
     const styleArr = [];
     let filesInfo = await fs.promises.readdir(dirPath);
-    filesInfo = filesInfo.filter((file) => path.parse(file).ext === ".css");
+    filesInfo = filesInfo.filter((file) => path.parse(file).ext === '.css');
 
     //Read files, in folder "styles"
     for (let i = 0; i < filesInfo.length; i++) {
       const filePath = `${dirPath}/${filesInfo[i]}`;
 
       const readStream = fs.createReadStream(filePath, { encoding: 'utf8' });
-      
+
       await new Promise((resolve, reject) => {
         readStream.on('data', (text) => {
           styleArr.push(text);
-        }); 
+        });
         readStream.on('end', () => resolve());
         readStream.on('error', (err) => reject(err));
       });
     }
-    
+
     const bundlePath = path.join(__dirname, 'project-dist/bundle.css');
     const writeStream = fs.createWriteStream(bundlePath, {
       flags: 'w',
       encoding: 'utf8',
     });
-    
+
     //Create and write file "bundle.css"
     await new Promise((resolve, reject) => {
-      writeStream.write(styleArr.join(""), 'utf8', (writeError) => {
+      writeStream.write(styleArr.join(''), 'utf8', (writeError) => {
         if (writeError) {
           reject(writeError);
         } else {
@@ -41,7 +41,7 @@ const dirPath = path.join(__dirname, 'styles');
         }
       });
     });
-  }catch (err) {
+  } catch (err) {
     console.error('Error:', err);
   }
-})()
+})();
